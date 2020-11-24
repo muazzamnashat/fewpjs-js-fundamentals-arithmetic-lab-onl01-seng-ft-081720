871b4e6a9158c8ac43f1a0e465e52bc1602ae248
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-11-23 23:37:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = (Math.random()*10)+ 1;
+const mod = 19 % 5;
+const max = Math.max(1,2,20);
